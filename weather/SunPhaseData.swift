e844a776e87a37ee872663e49cc7517f38043a5a
Andrey Timokhov <andreytimokhov@MacBook-Pro-Andrey.local>
2017-07-06 18:35:31 +0300

    1st version

---
+++ weather/SunPhaseData.swift
@@ -0,0 +1,16 @@
+//
+//  SunPhaseData.swift
+//  weather
+//
+//  Created by Andrey Timokhov on 06/07/2017.
+//  Copyright © 2017 Andrey Timokhov. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class SunPhaseData: Object  {
+    dynamic var Sunset: String = ""
+    dynamic var Sunrise: String = ""
+    
+}
--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let realm = try! Realm()
+        //print (Realm.Configuration.defaultConfiguration.fileURL)
+        var SunriseTime: String = ""
+        var SunsetTime: String = ""
+        
+        let url = "http://api.wunderground.com/api/0aaad10764e1a886/astronomy/q/Russia/Moscow.json"
 
+        
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            
+            case .success(let value):
+                let json = JSON(value)
+                
+                print("JSON: \(json["moon_phase"]["sunrise"]["hour"].stringValue)")
+                let todaySunPhase = SunPhaseData()
+                todaySunPhase.Sunset = json["moon_phase"]["sunset"]["hour"].stringValue + ":" + json["moon_phase"]["sunset"]["minute"].stringValue
+                todaySunPhase.Sunrise = json["moon_phase"]["sunrise"]["hour"].stringValue + ":" + json["moon_phase"]["sunrise"]["minute"].stringValue
+                print ("Sunset: \(todaySunPhase.Sunset)")
+                print ("Sunrise: \(todaySunPhase.Sunrise)")
+                try! realm.write {
+                    realm.add(todaySunPhase)
+                }
+            
+            case .failure(let error):
+                print("!!!ERROR!!!: \(error)")
+            } //switch
+        
+        }
+
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-}
+} //class ViewController: UIViewController
 
